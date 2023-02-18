a402e6e6cbd0f2b424136260431407a930516c88
LuisDogo <lmorenos2100@alumno.ipn.mx>
2023-02-17 18:46:19 -0600

    tictactoe.h has been modified

--- tictactoe.h
+++ tictactoe.h
@@ -2,19 +2,23 @@
 #include <utility>
 using namespace std;
 
-void player_turn(){
+
+
+
+void player_turn(char game[3][3]){
 
 }
 
-void bot_turn(){
-
+void bot_turn(char game[3][3]){
+    
 }
 
 void gameplay(){
     bool endgame = false;
+    char game[3][3];
     while (endgame == false)
     {
-        player_turn();
-        bot_turn();
+        player_turn(game);
+        bot_turn(game);
     }
 }
